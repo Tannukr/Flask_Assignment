5d204e41e892ed0b9eaa8ffaf54b0b573b0d3e60
Tannukr <tannu20114@gmail.com>
2025-09-02 19:57:10 +0530

    Updated Flask app and added PostgreSQL connection

--- config.py
+++ config.py
@@ -1,18 +1,28 @@
 import os
 from dotenv import load_dotenv
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 # Load .env file
 load_dotenv()
 
+app = Flask(__name__)
+
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "fallbacksecret")
-
-    SQLALCHEMY_DATABASE_URI = "sqlite:///students.db"
+    
+    # PostgreSQL connection URI
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        "postgresql://postgres:1234@localhost:5432/Flask_database"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+    # Mail settings
     MAIL_SERVER = "smtp.gmail.com"
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.getenv("MAIL_USERNAME")
     MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
     MAIL_DEFAULT_SENDER = ("Admin Team", os.getenv("MAIL_USERNAME"))
+
